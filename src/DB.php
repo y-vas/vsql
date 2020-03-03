7b1e75a44be3cacfc97d6ac4c5e84f62ccfcbc82
Vasyl Yovdiy <yovdiyvasyl@gmail.com>
2020-03-03 12:04:39 +0100

    utf

--- src/DB.php
+++ src/DB.php
@@ -56,6 +56,14 @@ class DB {
         $this->error('Unable to connect to the database!');
       }
 
+      $this->connect->query("
+        SET character_set_results = 'utf8',
+        character_set_client = 'utf8',
+        character_set_connection = 'utf8',
+        character_set_database = 'utf8',
+        character_set_server = 'utf8'
+      ");
+
       return $this->connect;
     }
 
