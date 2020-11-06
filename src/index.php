27e5ee8770aabc1c487cb632d83fbc480e831f5a
Vasyl Yovdiy <yovdiyvasyl@gmail.com>
2020-11-06 12:21:24 +0100

    auto push 2020-11-06 12:21:24.482473

--- src/index.php
+++ src/index.php
@@ -3,11 +3,11 @@ include('Mold.php');
 
 
 $_ENV['VSQL_INSPECT'] = true;
-$_ENV[  'DB_HOST'  ] = '172.19.0.2:8080';
+$_ENV[  'DB_HOST'  ] = '172.19.0.2';
 $_ENV['DB_USERNAME'] = 'root';
 $_ENV['DB_PASSWORD'] = 'root';
 $_ENV['DB_DATABASE'] = 'dbtest';
 
 
 $m = new Mold();
-$m.makeMold('test');
+$m->makeMold('users');
