f2366e820cc7b0881dd441276411f50517de9d6a
Vasyl <yovdiyvasyl@gmail.com>
2021-01-29 10:21:24 +0100

    debug

--- src/Mold.php
+++ src/Mold.php
@@ -1,7 +1,7 @@
 <?php
 
 // namespace VSQL\VSQL;
-require(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'DB.php');
+include(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'DB.php');
 
 class Mold extends DB {
 
