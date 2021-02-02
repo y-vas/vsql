fa810236dd096174e258f0363852115e3cad9ff4
Vasyl Yovdiy <yovdiyvasyl@gmail.com>
2021-02-02 08:09:24 +0100

    myslq container

--- web/index.php
+++ web/index.php
@@ -3,7 +3,7 @@ require_once '../src/VSQL.php';
 use VSQL\VSQL\VSQL;
 
 $_ENV['VSQL_INSPECT'] = true;
-$_ENV[  'DB_HOST'  ] = '172.29.0.2';
+$_ENV[  'DB_HOST'  ] = 'mysql_vsql';
 $_ENV['DB_USERNAME'] = 'root';
 $_ENV['DB_PASSWORD'] = 'root';
 $_ENV['DB_DATABASE'] = 'dbtest';
